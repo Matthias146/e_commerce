7dd7792e3262d861ac0110683e6945595ca5dcfe
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-08-10 21:28:18 +0200

    feat(spring:config): added a helper function to get id's from spring-data-rest

--- server/ecommerce/src/main/java/com/spring/ecommerce/config/DataRestConfig.java
+++ server/ecommerce/src/main/java/com/spring/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,28 @@ package com.spring.ecommerce.config;
 
 import com.spring.ecommerce.entity.Product;
 import com.spring.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+	private final EntityManager entityManager;
+
+	@Autowired
+	public DataRestConfig(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -34,5 +48,19 @@ public class DataRestConfig implements RepositoryRestConfigurer {
 				.withCollectionExposure(
 						(metdata, httpMethods) ->
 								httpMethods.disable(unsupportedActions));
+
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+		List<Class<?>> entityClasses = new ArrayList<>();
+		for (EntityType<?> tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+		Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 }
