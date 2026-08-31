79b2b45a1f848ceeedc675e680bf7bb2fc5d6b79
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-08-31 21:49:17 +0200

    feat(OAuth): init OAUTH with simple test routes to connect

---
+++ server/ecommerce/src/main/java/com/spring/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.spring.ecommerce.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                       // .requestMatchers("/api/products/**").permitAll()
+                      //  .requestMatchers("/api/product-category/**").permitAll()
+                       // .requestMatchers("/api/countries/**").permitAll()
+                      //  .requestMatchers("/api/states/**").permitAll()
+                        .requestMatchers("/api/checkout/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
+}
