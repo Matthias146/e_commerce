3548d249a1b56a7e4417fcffe70f203b8735577c
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-09-06 15:18:40 +0200

    refactor(styles): refactor styles in Order-Form

--- client/e-commerce/src/app/features/checkout/validators/checkout.schema.ts
+++ client/e-commerce/src/app/features/checkout/validators/checkout.schema.ts
@@ -1,20 +1,20 @@
-import { required, SchemaPathTree } from '@angular/forms/signals';
+import { email, required, SchemaPathTree } from '@angular/forms/signals';
 import { CustomerFormModel } from '../data/models/customerFormModel.interface';
 
 export const checkoutSchema = (path: SchemaPathTree<CustomerFormModel>): void => {
   required(path.contact.firstName, { message: 'First Name is required' });
-  required(path.contact.lastName);
-  required(path.contact.email);
+  required(path.contact.lastName, { message: 'Last Name is required' });
+  required(path.contact.email, { message: 'Email Name is required' });
+  email(path.contact.email, { message: 'Enter a valid email address' });
+  required(path.shippingAddress.street, { message: 'First Name is required' });
+  required(path.shippingAddress.city, { message: 'First Name is required' });
+  required(path.shippingAddress.state, { message: 'First Name is required' });
+  required(path.shippingAddress.country, { message: 'First Name is required' });
+  required(path.shippingAddress.zipCode, { message: 'First Name is required' });
 
-  required(path.shippingAddress.street);
-  required(path.shippingAddress.city);
-  required(path.shippingAddress.state);
-  required(path.shippingAddress.country);
-  required(path.shippingAddress.zipCode);
-
-  required(path.billingAddress.street);
-  required(path.billingAddress.city);
-  required(path.billingAddress.state);
-  required(path.billingAddress.country);
-  required(path.billingAddress.zipCode);
+  required(path.billingAddress.street, { message: 'First Name is required' });
+  required(path.billingAddress.city, { message: 'First Name is required' });
+  required(path.billingAddress.state, { message: 'First Name is required' });
+  required(path.billingAddress.country, { message: 'First Name is required' });
+  required(path.billingAddress.zipCode, { message: 'First Name is required' });
 };
